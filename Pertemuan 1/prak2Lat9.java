abc77aad5e8a3c89ebdfc0f85d7258f2cd620e22
fernando123241 <66262381+fernando123241@users.noreply.github.com>
2021-01-03 23:41:35 +0700

    Add files via upload

---
+++ Pertemuan 1/prak2Lat9.java
@@ -0,0 +1,13 @@
+public class prak2Lat9 {
+public static void main(String[] args) {
+int number=13;
+if(number%2==0){
+System.out.println("Angka genap");
+}else{
+System.out.println("Angka ganjil");
+}
+}
+}
+/*
+ Program diatas untuk mencoba seleksi if-else
+ */
