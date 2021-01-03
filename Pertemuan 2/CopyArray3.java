b268334d9a0fdac76a4cef0715454de4280a41f7
fernando123241 <66262381+fernando123241@users.noreply.github.com>
2021-01-04 00:10:20 +0700

    Add files via upload

---
+++ Pertemuan 2/CopyArray3.java
@@ -0,0 +1,11 @@
+public class CopyArray3{
+	public static void main(String args[]) {
+		String str1 = "Informatika Universitas Syiah Kuala";
+		System.out.println(str1.startsWith("Inf"));
+		System.out.println(str1.endsWith("kuala"));
+		System.out.println(str1.startsWith("Inf",1));
+		System.out.println(str1.startsWith("Inf",2));
+		System.out.println(str1.startsWith("Inf",3));
+	}
+}/*kode di atas bertujuan untuk mengecek string dan membadingkan dengan yang ada
+pada str1 . jika ada/sama maka true, jika tidak ada maka false*/
