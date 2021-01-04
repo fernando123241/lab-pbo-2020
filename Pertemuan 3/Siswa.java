447c0ebe719081ba8a2b0385d4dca7c9f156c713
fernando123241 <66262381+fernando123241@users.noreply.github.com>
2021-01-04 14:58:32 +0700

    Add files via upload

---
+++ Pertemuan 3/Siswa.java
@@ -0,0 +1,12 @@
+public class Siswa { 
+	private int nrp; 
+	public Siswa(int nrpx){ /* Method Constructor */
+		nrp = nrpx;
+	}
+	public void setNrp(int nrpx){ /*Method Accestor */
+		nrp = nrpx;
+	}
+	public int getNrp(){ /* MEthod Mutattor */
+		return nrp;
+	}
+}
